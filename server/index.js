1ee239763d486c9f3b87622eab51875d1226dacb
A Easterwood <aliciaeasterwood@gmail.com>
2019-06-29 13:15:41 -0500

    modifiy package.json

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const path = require('path');
 const port = process.env.HTTP_PORT || 3001;
 
 const app = express();
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-app.use('/:id', express.static(__dirname + '/../client/dist'));
+app.use('/:id', express.static(path.join(__dirname + '/../client/dist')));
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
